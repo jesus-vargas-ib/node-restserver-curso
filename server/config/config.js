6f8eedc6de3f796a40afb3e1a6b12fe50175d1c9
Jesus Vargas <jesus.vargas@codes.com.mx>
2018-12-24 13:41:35 -0600

    Variable de entorno Mongo PRD

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-usr:JdaHNSg8yAnzwZ2@ds239965.mlab.com:39965/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
